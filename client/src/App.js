50be5fa61b1c6d596edb4df8f43c46021d0193bf
Shakti <shaktirajbhandari@gmail.com>
2023-08-02 13:52:17 +0545

    garage

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,16 @@
 import Title from "./components/Title";
 import Example from "./components/Example";
+
 function App({name}) {
+  const cars= ["BMW",'Ferrari', 'Mercedes'];
   return (
     <>
       <Title name = "TODO App" />
-      <Example/>
+      {/* <Example isGoal = {true}/> */}
+      <Example cars={cars}/>
     </>
   );
 };
+
 
 export default App;
--- client/src/components/Example.js
+++ client/src/components/Example.js
@@ -1,14 +1,58 @@
 //rfce
 
 //Adding Events to Component
-export default function Example() {
-    const shoot = () => {
-        alert("goal");
-    }
-  return (
-    <>
-        <button onClick = {shoot}>Take the shot!</button>
-    </>
-  );
+// export default function Example() {
+//     const shoot = () => {
+//         alert("goal");
+//     }
+//   return (
+//     <>
+//         <button onClick = {shoot}>Take the shot!</button>
+//     </>
+//   );
+// }
+
+// export default function Football() {
+//     const shoot = (a) => { 
+//         alert(a); 
+//     }
+//     return (
+//         <>
+//             <button Onclick = {() => shoot("Goal!")}>Take the Shot!</button>
+//         </>
+//     );
+// }
+
+// export default function Goal(props) {
+//     const isGoal = props.isGoal;
+//     if(isGoal){
+//         return <MadeGoal/>;
+//         }
+//         return <MissedGoal/>;
+//     }
+
+// function MissedGoal(){
+//     return <h1>MISSED!</h1>
+// }
+
+// function MadeGoal(){
+//     return <h1>Goal!</h1>
+// }
+
+const cars = ['Ford', 'BMW', 'Audi'];
+
+<Garage cars = {cars}/>
+ export default function Garage(props) {
+    const cars = props.cars;
+    return (
+        <>
+            <h1> Garage</h1>
+            {cars.length > 0 &&
+            <h2>
+                You have {cars.length} cars in your garage.
+            </h2>
+            }
+        </>
+    )
 }
 
